acebdc4f4ec4aa23c4558ace163ca378e8ede802
Kyle Kelley <rgbkrk@gmail.com>
2026-05-01 18:29:33 -0700

    chore(plugins): regenerate isolated-renderer LFS bundles
    Pick up the fresh isolated-renderer output from this branch's rebuild.
    Pointers update to the new LFS OIDs; bundles stay LFS-tracked per #2464.

--- apps/notebook/src/renderer-plugins/isolated-renderer.js
+++ apps/notebook/src/renderer-plugins/isolated-renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46edbd839856342f983609f07b9eb1e97d823044a9ad93722aa5f688600e1dbc
-size 1475186
+oid sha256:fccc32117460ec66b040308e1e19fd4a436d095e3f869e200772bd0c50ecddfe
+size 1475652
